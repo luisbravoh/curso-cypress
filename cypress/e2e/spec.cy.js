79e1a33529f3aff8a8d349470c017a263d0bf2ea
luisbravoh <luisbravoh@gmail.com>
2023-08-14 18:08:29 -0400

    creacion de pagina de signup

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,13 +1,16 @@
 import { LoginMethods } from "./pages/login/login.methods"
+import { SignupMethods } from "./signup/signup.methods"
 
 describe('template spec', () => {
   it('passes', () => {
-    const usuario = 'luis42'
-    const contrasena = 'luis42'
+    const usuario = 'luis420'
+    const contrasena = 'luis420'
     cy.visit('https://demoblaze.com/index.html')
-    cy.get('a[data-target="#logInModal"]').click()
-    LoginMethods.login(usuario, contrasena)
-    cy.get('a#nameofuser').should('contain.text', usuario)
+    //cy.get('a[data-target="#logInModal"]').click()
+    cy.get('a[data-target="#signInModal"]').click();
+    SignupMethods.signup(usuario, contrasena);
+    //LoginMethods.login(usuario, contrasena)
+    //cy.get('a#nameofuser').should('contain.text', usuario)
 
     cy.wait(10000)
   })
