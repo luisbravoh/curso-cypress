696646f27ec67f11cb1e6fec5983c28241a1e596
luisbravoh <luisbravoh@gmail.com>
2023-08-20 18:57:05 -0400

    Agregar logger

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   retries: 1,
+  //video: false,
   e2e: {
     setupNodeEvents(on, config) {
      // on('file:preprocessor', webpackPreprocessor);
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,16 +1,28 @@
 import { LoginMethods } from "./pages/login/login.methods"
 import { SignupMethods } from "./signup/signup.methods"
+import { Logger } from "./util/logger"
 
 describe('template spec', () => {
   it('passes', () => {
     const usuario = 'luis420'
     const contrasena = 'luis420'
+   
+    Logger.stepNumber(1)
+    Logger.step('Navigate to demoblaze page')
     cy.visit('https://demoblaze.com/index.html')
+
+    Logger.stepNumber(2)
+    Logger.step('Click on Login')
+    cy.get('a[data-target="#logInModal"]').click();
+
+    Logger.stepNumber(3)
+    Logger.step(`Login with this credentials: ${usuario}/${contrasena}`)
+    LoginMethods.login(usuario, contrasena)
+    Logger.verification(`The home should show "Welcome ${usuario} text`)
+    cy.get('a#nameofuser').should('contain.text', usuario)
     //cy.get('a[data-target="#logInModal"]').click()
-    cy.get('a[data-target="#signInModal"]').click();
-    SignupMethods.signup(usuario, contrasena);
-    //LoginMethods.login(usuario, contrasena)
-    //cy.get('a#nameofuser').should('contain.text', usuario)
+    
+    
 
     cy.wait(10000)
   })
